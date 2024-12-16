b86441a3acde0c7fc4be8ea56fd715cc75b9e15d
Whitzzscott <samytplayz197@gmail.com>
2024-12-16 20:24:22 +0800

    Update HomePage.jsx

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -4,6 +4,7 @@ import SearchBar from "../SearchBar/SearchBar";
 import CharacterCard from "../CharacterCard/CharacterCard";
 import CharacterCardModal from "../CharacterCardModal/CharacterCardModal";
 import "./HomePage.css";
+import Footer from "../../Assets/Footer.png";
 
 const HomePage = () => {
   const [characters, setCharacters] = useState([]);
@@ -94,6 +95,37 @@ const HomePage = () => {
         </div>
       </div>
 
+      <div className="text-center mt-3">
+        <a
+          href="http://agaga.com"
+          className="d-inline-block rounded overflow-hidden shadow-lg transition-transform duration-300 ease-in-out transform hover:scale-105"
+          style={{
+            fontWeight: "bold",
+            userSelect: "none",
+            WebkitUserSelect: "none",
+            MozUserSelect: "none",
+            msUserSelect: "none",
+          }}
+        >
+          <img
+            src={Footer}
+            alt="Footer Image"
+            className="rounded"
+            style={{
+              maxWidth: "40%",
+              height: "auto",
+              borderRadius: "20px",
+              filter: "brightness(1.1)",
+              padding: "8px",
+              userSelect: "none",
+              WebkitUserSelect: "none",
+              MozUserSelect: "none",
+              msUserSelect: "none",
+            }}
+          />
+        </a>
+      </div>
+
       <h1
         style={{
           position: "absolute",
@@ -109,31 +141,14 @@ const HomePage = () => {
       >
         Pandamonium
       </h1>
+
       <div>
-        <SearchBar />
         <div>
           <div
             className="search-section d-flex justify-content-center align-items-center mb-4"
             style={{ fontSize: "1.2rem" }}
           >
-            <input
-              type="text"
-              className="search-input form-control me-2"
-              placeholder="Search characters..."
-              aria-label="Search characters"
-              style={{ padding: "8px", width: "80%" }}
-              onChange={(e) => {
-                const query = e.target.value;
-                if (query.length > 0) {
-                  const filtered = characters.filter((character) =>
-                    character.name.toLowerCase().includes(query.toLowerCase()),
-                  );
-                  setFilteredCharacters(filtered);
-                } else {
-                  setFilteredCharacters([]);
-                }
-              }}
-            />
+            <SearchBar />
             <button
               className="search-button btn btn-primary"
               style={{ padding: "8px 16px", fontSize: "1rem" }}
