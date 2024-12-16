232891df9836fac96d1eac6e5ab16675ec48c524
Whitzzscott <samytplayz197@gmail.com>
2024-12-16 20:24:52 +0800

    Update SearchBar.jsx

--- src/Components/SearchBar/SearchBar.jsx
+++ src/Components/SearchBar/SearchBar.jsx
@@ -1,5 +1,13 @@
 const SearchBar = ({ performSearch }) => {
-  return <input type="text" onChange={(e) => performSearch(e.target.value)} />;
+  return (
+    <input
+      type="text"
+      className="search-input form-control me-2"
+      placeholder="Search characters..."
+      aria-label="Search characters"
+      onChange={(e) => performSearch(e.target.value)}
+    />
+  );
 };
 
 export default SearchBar;
