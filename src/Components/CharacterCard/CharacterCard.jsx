3afa38a235b0a526ae5b8c3feb62264c331cd683
William Pieh <wtpieh@gmail.com>
2024-12-16 15:30:50 -0700

    Added the css styles responsible for rendering the hover animation.

--- src/Components/CharacterCard/CharacterCard.jsx
+++ src/Components/CharacterCard/CharacterCard.jsx
@@ -1,33 +1,20 @@
+import React, { useRef } from "react";
 import "./CharacterCard.css";
-import React, { useState } from "react";
-import CharacterCardModal from "../CharacterCardModal/CharacterCardModal";
 
 const CharacterCard = ({ character }) => {
-  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
-  const [showModal, setShowModal] = useState(false);
+  const cardRef = useRef(null);
 
   const handleMouseMove = (e) => {
-    const rect = e.currentTarget.getBoundingClientRect();
-    setMousePosition({
-      x: e.clientX - rect.left, // X position relative to card
-      y: e.clientY - rect.top, // Y position relative to card
-    });
+    const rect = cardRef.current.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    cardRef.current.style.setProperty("--x", `${x}px`);
+    cardRef.current.style.setProperty("--y", `${y}px`);
   };
 
-  const truncatedDesc =
-    character.description.length > 100
-      ? character.description.substring(0, 100) + "..."
-      : character.description;
-
   return (
-    <div
-      className="character-card animate__animated animate__fadeInUp"
-      onMouseMove={handleMouseMove}
-      style={{
-        "--mouse-x": `${mousePosition.x}px`,
-        "--mouse-y": `${mousePosition.y}px`,
-      }}
-    >
+    <div ref={cardRef} className="character-card" onMouseMove={handleMouseMove}>
       <div className="image-container">
         <img
           className="character-image"
@@ -35,16 +22,9 @@ const CharacterCard = ({ character }) => {
           alt={character.name}
         />
       </div>
-      <div className="chat-count">
-        <i className="fas fa-comment"></i>
-        <span className="count">{character.chats}</span>
-      </div>
       <h3 className="character-name">{character.name}</h3>
-      <p className="character-description">{truncatedDesc}</p>
+      <p className="character-description">{character.description}</p>
       <span className="badge bg-primary character-tags">{character.tag}</span>
-      <div onClick={() => setShowModal(!showModal)}>
-        {showModal && <CharacterCardModal character={character} />}
-      </div>
     </div>
   );
 };
--- src/Components/CharacterCardModal/CharacterCardModal.jsx
+++ src/Components/CharacterCardModal/CharacterCardModal.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 
-const CharacterCardModal = ({ character, onHover }) => {
+const CharacterCardModal = ({ character }) => {
   return (
     <div className="character-modal">
       <div className="modal-content">
